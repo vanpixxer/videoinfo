182d7b6d7b40d1a87e1857a4b08f1b181651c56e
Darrell <webtastic.dc@gmail.com>
2018-12-19 19:54:50 -0800

    Completed simple Electron app

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const electron = require('electron');
+const ffmpeg = require( 'fluent-ffmpeg' );
 
-const { app, BrowserWindow } = electron;
+//import ipc main object from Electron
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow
 
 //add event handler to watch app object
 //can create new browser window inside of app.on() event handler
 app.on( 'ready', () => {
-  const mainWindow = new BrowserWindow({});
+ mainWindow = new BrowserWindow({});
   //instruct browser window to load HTML document
   mainWindow.loadURL(`file://${__dirname}/index.html`);
   //dirname is a global variable provided by Node.js
+});
+
+//listen for message, video:submit from index.html
+ipcMain.on( 'video:submit', (event, path) => {
+    //get duration of video files
+    ffmpeg.ffprobe(path, (err, metadata) => {
+         mainWindow.webContents.send('video:metadata', metadata.format.duration);
+    });
 });
