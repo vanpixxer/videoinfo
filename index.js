263250d8a1a8ce507e4edcf626ff62d1d8d53503
Darrell <webtastic.dc@gmail.com>
2018-12-19 17:20:45 -0800

    Succeeded in getting Electron to load a browser window

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+//add event handler to watch app object
+//can create new browser window inside of app.on() event handler
+app.on( 'ready', () => {
+  const mainWindow = new BrowserWindow({});
+  //instruct browser window to load HTML document
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  //dirname is a global variable provided by Node.js
+});
